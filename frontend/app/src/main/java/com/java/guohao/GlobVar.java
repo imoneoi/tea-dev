91f8dfc2d35c40667e59423074aec317b6f86d92
郭昊 <2019013292@secoder.net>
2021-08-15 14:51:32 +0800

    Add global variables

--- frontend/app/src/main/java/com/java/guohao/GlobVar.java
+++ frontend/app/src/main/java/com/java/guohao/GlobVar.java
@@ -2,4 +2,9 @@ package com.java.guohao;
 
 public class GlobVar {
     public static final int BOTTOM_NAV_LENGTH = 4;
+    public static final String API_ADDR = "http://127.0.0.1:8000/api/";
+    public static final String LOGIN_ADDR = API_ADDR + "login";
+    public static final String REGISTER_ADDR = API_ADDR + "register";
+    public static final String USERDATA_ADDR = API_ADDR + "userdata";
+    public static final String PROC_ADDR = API_ADDR + "proc";
 }
