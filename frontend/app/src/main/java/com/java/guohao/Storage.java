c2adc86974443e4ac16d9e950091e845f2e50418
郭昊 <2019013292@secoder.net>
2021-08-17 10:58:48 +0800

    Add find to storage class

--- frontend/app/src/main/java/com/java/guohao/Storage.java
+++ frontend/app/src/main/java/com/java/guohao/Storage.java
@@ -4,15 +4,22 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public class Storage {
+    private static final String SP_NAME = "storage";
+
     public static boolean save(Context context, String key, String value) {
-        SharedPreferences sp = context.getSharedPreferences("storage", Context.MODE_PRIVATE);
+        SharedPreferences sp = context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
         editor.putString(key, value);
         return editor.commit();
     }
 
+    public static boolean contains(Context context, String key) {
+        SharedPreferences sp = context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE);
+        return sp.contains(key);
+    }
+
     public static String load(Context context, String key) {
-        SharedPreferences sp = context.getSharedPreferences("storage", Context.MODE_PRIVATE);
+        SharedPreferences sp = context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE);
         return sp.getString(key, "");
     }
 }
