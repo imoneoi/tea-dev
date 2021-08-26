47d3dea035ec1f4426875d4da919a97524ea1995
TerryHu <humuyan@163.com>
2021-08-26 14:27:17 +0800

    Improve back press logic, add icon for me fragment

--- frontend/app/src/main/java/com/java/guohao/MeFragment.java
+++ frontend/app/src/main/java/com/java/guohao/MeFragment.java
@@ -2,35 +2,46 @@ package com.java.guohao;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import java.util.Objects;
-
 public class MeFragment extends Fragment {
 
-    final String[] menu_text = {"我的收藏", "浏览历史", "外观", "退出登录"};
+    final String[] menu_text = {"我的收藏", "浏览历史", "设置", "退出登录"};
+    final int[] icon_id = {
+            R.drawable.star_border,
+            R.drawable.ic_baseline_history_24,
+            R.drawable.ic_baseline_settings_24,
+            R.drawable.baseline_logout_24
+    };
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView mText;
+        private final ImageView mImage;
         private final View mView;
 
         public ViewHolder(View view) {
             super(view);
             mText = view.findViewById(R.id.menu_item_text);
+            mImage = view.findViewById(R.id.item_icon);
             mView = view;
         }
 
         public TextView getText() {
             return mText;
+        }
+
+        public ImageView getImage() {
+            return mImage;
         }
 
         public View getView() {
@@ -54,6 +65,7 @@ public class MeFragment extends Fragment {
             @Override
             public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
                 holder.getText().setText(menu_text[position]);
+                holder.getImage().setImageResource(icon_id[position]);
                 holder.getView().setOnClickListener(v -> {
                     Class next_page = null;
                     switch (position) {
@@ -61,6 +73,7 @@ public class MeFragment extends Fragment {
                             HttpUtils.user.session = "";
                             ((MainActivity) requireActivity()).saveSession();
                             next_page = Login.class;
+                            Toast.makeText(v.getContext(), "成功登出", Toast.LENGTH_LONG).show();
                             break;
                     }
                     Intent intent = new Intent(v.getContext(), next_page);
